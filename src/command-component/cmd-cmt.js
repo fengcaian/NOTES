0204f317b0df895011f93d62e7623e6bd822f154
fengcaian <caianfeng@yeah.net>
2018-01-14 22:07:15 +0800

    cc-button

--- src/command-component/cmd-cmt.js
+++ src/command-component/cmd-cmt.js
@@ -4,26 +4,36 @@ define([''], function (){
         return {
             restrict: 'E',
             replace: true,
-            template: '<div class="col-xs-2">\n' +
-            '    <select id="select" class="form-control">\n' +
-            '        <option ng-repeat="o in option" value="{{o.value}}">{{o.label}}</option>\n' +
+            template: '<div class="col-xs-{{column || 1}}">\n' +
+            '    <select class="form-control">\n' +
+            '        <option ng-repeat="o in option" id="{{o.id}}" value="{{o.value}}">{{o.label}}</option>\n' +
             '    </select>\n' +
             '</div>',
             scope: {
                 option: '=?',
                 change: '=',
-                disabled: '='
+                disabled: '=',
+                selectedId: '=',
+                column: '='
             },
             controller: function ($scope) {
                 console.log($scope);
             },
             link: function (scope, element) {
+                scope.column = typeof(scope.column-0) === 'number' ? scope.column-0 : 1;
                 var isDisabled = scope.disabled || false;
                 var select = angular.element(element);
+                //var op = document.getElementById('id').options;
                 select.find('select').attr('disabled',isDisabled);
+                /*for (var i=0, l=op.length; i<l; i++) {
+                    var o = op[i];
+                    if (scope.selectedId === o.id) {
+                        o.selected = true;
+                    }
+                }*/
                 element.on('change', 'select',  function(e){
                     var oSelected = select.find("option:selected");
-                    scope.change && scope.change({
+                    scope.change && scope.change.call(this, e, {
                         label: oSelected.text(),
                         value: oSelected.val()
                     });
@@ -35,12 +45,15 @@ define([''], function (){
         return {
             restrict: 'E',
             replace: true,
-            template: '',
+            template: '<button type="{{option.type}}" class="btn btn-{{option.bClass}} btn-{{option.size}}">{{option.value}}</button>',
             scope: {
-
+                option: '='
             },
             link: function (scope, element) {
-
+                typeof scope.option.disabled === 'boolean' && scope.option.disabled && element.attr('disabled', 'disabled');
+                element.bind('click', function (e){
+                    scope.option.click && scope.option.click.call(this, e);
+                })
             }
         }
     });
--- src/framework.js
+++ src/framework.js
@@ -5,24 +5,41 @@ define(['src/command-component/cmd-cmt'], function (cc) {
            placeholder: '-- 请选择 --',
            value: '',
            disabled: false,
+           selectedId : '',
+           column: '3',
            option: [
                {
+                   id: '1',
                    label: '篮球',
                    value: 1
                },
                {
+                   id: '2',
                    label: '足球',
-                   value: 2
+                   value: '2'
                },
                {
+                   id: '3',
                    label: '排球',
                    value: 3
                }
            ],
-           change: function (option){
+           change: function (e, option){
+               console.log(this);
                console.log(option);
            }
        };
+       $scope.btn = {
+           type: 'button',
+           bClass: 'primary',
+           size: 'lg',
+           value: '按钮',
+           disabled: false,
+           click: function (e) {
+               console.log(this.type);
+               console.log(e);
+           }
+       }
    }]);
    return module;
 });
